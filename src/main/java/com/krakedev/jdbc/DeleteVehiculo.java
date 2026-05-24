0829926275a50c55019d220edd029a6bea577c3b
2Erik <erik19yogo@gmail.com>
2026-05-24 07:24:19 -0500

    ERT: DeleteVehiculo funcionando

---
+++ src/main/java/com/krakedev/jdbc/DeleteVehiculo.java
@@ -0,0 +1,46 @@
+package com.krakedev.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DeleteVehiculo {
+	
+	private static final Logger LOGGER = LogManager.getLogger(DeleteVehiculo.class);
+
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = """
+				DELETE FROM vehiculos
+				WHERE placa = ?
+				""";
+		
+		try {
+			con = Conexion.getConnection();
+			ps = con.prepareStatement(sql);
+			
+			ps.setString(1, "PBD-4789");
+			
+			int filas = ps.executeUpdate();
+			LOGGER.info("Filas eliminadas: "+filas);
+			LOGGER.info("Vehiculo eliminado");
+		} catch (Exception e) {
+			
+			LOGGER.error("Error al eliminar vehiculo "+e.getMessage());
+		}finally {
+			try {
+				if(con!=null) {
+					con.close();
+				}
+			} catch (Exception e2) {
+				LOGGER.error("Error al cerrar la conexion: "+e2.getMessage());
+			}
+		}
+
+	}
+
+}
